d7c0fb596810ae0a6e91c9ece5f0abeaeb80ca56
Agrima Singh <73841853+agrima-singh@users.noreply.github.com>
2021-10-20 16:45:25 +0530

    Add files via upload

---
+++ 1st chapter.py
@@ -0,0 +1,54 @@
+#Pyhton is a Dynamic Programming Language
+#In python we can use single and double quotes for string but in java double quote is used for string and single is used for character.
+#print() function
+print("Hello")
+
+# we can use "double quotes" inside 'single quotes' and 'single quotes' inside "double quotes"
+# we can't use 'single quotes' inside 'single quotes' and "double quotes" inside "double quotes"
+print("Hello 'Agrima Singh'")
+print('Hello "Agrima Singh"')
+
+
+# Escape Sequences
+# \" , \' , \n , \t , \\ , \b
+print("Hello \"Agrima SIngh\" ") 
+print('Hello \'Agrima Singh\' ')
+print("Hello \n Agrima Singh")      # new line
+print("Hello \t Agrima Singh")      # tab
+print("Hello \b Agrima Singh")      # backspace
+print("Hello  Agrima\\")            # if we want to print single backslash
+
+# Raw Strings
+print(r"Hello \n Agrima Singh")     # by using r we can make it a normal string
+
+# Calculator
+print(2+3)     #addition
+print(3-2)      #substraction
+print(44*2)      # multiplication
+print(4/2)        # float division
+print(4//2)        #integer division
+print(2**3)         #2^3
+print(2**0.5)
+#if we want to round a large value
+print(round(2**0.5,4))       #it will print four values after point
+
+#Precedence rule & Associativity rule
+# () >> ** >> * >> \ >> \\ >> % >> + >> -
+
+#Variables: Used to store data
+number1=2
+print(number1)
+number2= number1 + 5
+print(number2)
+
+#variable naming rule
+# 1. We can't start variable name with number but we can use it in iddle
+# 2. We can start it with any letter and _ (underscore) 
+# 3. We can't use special symbol while naming variable
+
+#Naming comvertions
+#user_name    (Snake Case Writting in Python)
+#userName     (Camel Case Writting in Java)
+
+
+
